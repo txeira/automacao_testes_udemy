e1a2d1f6243da91a0850bc642764584a5cd339cd
fabianoissa@gmail.com <fabianoissa@gmail.com>
2021-08-23 22:48:30 -0300

    Update Web.java

--- src/test/java/support/Web.java
+++ src/test/java/support/Web.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
-
 import java.io.File;
 
 public class Web {
